ebe43665f43a9695841679cf44c9eeed3a5e850b
Sana Siddiqui <sana.siddiqui115@gmail.com>
2026-05-19 15:08:47 +0200

    testing pipeline with orrect trust policy

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "demo" {
-  bucket = "ironhack-terraform-demo-applepie"
+  bucket = "ironhack-terraform-demo-apples"
 
   tags = {
     Name = "terraform-demo"
