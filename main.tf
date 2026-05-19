08425a419b8a9c9c82660c15bcb9f8ff07ead922
Sana Siddiqui <sana.siddiqui115@gmail.com>
2026-05-19 15:05:37 +0200

    created base infrastructure

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">= 1.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "demo" {
+  bucket = "ironhack-terraform-demo-applepie"
+
+  tags = {
+    Name = "terraform-demo"
+    Env  = "dev"
+  }
+}
