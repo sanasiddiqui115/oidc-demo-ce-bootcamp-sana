de46b5f803e0279403ce64e28790357fa268210c
Sana Siddiqui <sana.siddiqui115@gmail.com>
2026-05-20 14:56:29 +0200

    made a change to test pr checklist

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "demo" {
-  bucket = "ironhack-terraform-demo-apple"
+  bucket = "ironhack-terraform-demo-applepie"
 
   tags = {
     Name = "terraform-demo"
